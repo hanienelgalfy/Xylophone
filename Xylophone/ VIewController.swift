4c813af110e57f150f5cd5d715a0c2292b7194ec
Hanien ElGalfy <hanienelgalfy@Haniens-MacBook-Air.local>
2019-09-25 16:55:54 +0200

    adding all the view in a collection outlet

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    @IBOutlet var collection: [UIView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       for item in collection{
+        item.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(playsound)))
+       }
+    }
+    @objc func playsound(){
+        print("hi")
+    }
+    
+
+}
+
