d9efaa74212c84a87053db668b70c6d4ae82584e
Hanien ElGalfy <hanienelgalfy@Haniens-MacBook-Air.local>
2019-09-28 16:13:36 +0200

    Adding a seprate function that make the sound plays

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -20,40 +20,38 @@ class ViewController: UIViewController{
         super.viewDidLoad()
         
        for item in collection{
-        item.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.playsound(_:))))
+        item.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.notePressed(_:))))
         }
     }
-    @objc func playsound(_ sender : UITapGestureRecognizer){
+    @objc func notePressed(_ sender : UITapGestureRecognizer){
         
         guard let viewTag = sender.view?.tag else{
          return
         }
+        playsound(viewTag: viewTag)
       
-         guard let url = Bundle.main.url(forResource: "note\(viewTag)", withExtension: "wav") else { return }
         
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            /* iOS 10 and earlier require the following line:
-             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
-            
-            guard let player = player else { return }
-            
-            player.play()
-            
-        } catch let error {
-            print(error.localizedDescription)
-        }
-        
-        
-                //using tagss is left (_ sender: UIView)
-        
+     
     }
-    
+    func playsound(viewTag : Int){
+        guard let url = Bundle.main.url(forResource: "note\(viewTag)", withExtension: "wav") else { return }
+               
+               do {
+                   try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                   try AVAudioSession.sharedInstance().setActive(true)
+                    
+                   player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue) //the player is like a radio and we enter the CD in it which is the url
+                 
+                   
+                   guard let player = player else { return }
+                   
+                   player.play()
+                   
+               } catch let error {
+                   print(error.localizedDescription)
+               }
+    }
+
 
 }
 
